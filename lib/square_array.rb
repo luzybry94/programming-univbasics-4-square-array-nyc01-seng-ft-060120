359e5241597969eaf7ada7e80293c8b8469c0304
Chaim Bryski <luzybry94@gmail.com>
2020-05-05 22:26:21 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # your code here
-  new_array = []
-  array.length.times do |element|
+    new_array = []
+    array.length.times do |element|
     new_array << array[element] ** 2
   end
 end
