bdadee74c141a1bffbfd7e77f5e1ea4eebd347a0
Chaim Bryski <luzybry94@gmail.com>
2020-05-05 22:23:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,12 @@
 def square_array(array)
   # your code here
   new_array = []
-  array.length.times 
+  array.length.times do |element|
+    new_array << element ** 2
 end
+
+# 
+
+#length.times do |index|
+ # puts array[index]
+#end
